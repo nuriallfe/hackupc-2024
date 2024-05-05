f144e0cf7ad280e1af1395ab8f1dd9433bbc314a
caiselvass <caiselvassala@gmail.com>
2024-05-05 02:54:58 +0200

    ADD city texts

--- extract_landmarks.py
+++ extract_landmarks.py
@@ -1,6 +1,6 @@
 from landmark_class import CreateDataLandmarks
 
-landmarks_file = "./data/emblematic_european_sites.inp"
+landmarks_file = "./data/landmark_names.inp"
 save_dir = "./data"
 from_csv = "./data/data.csv"
 cd = CreateDataLandmarks(landmarks_file, save_dir)
